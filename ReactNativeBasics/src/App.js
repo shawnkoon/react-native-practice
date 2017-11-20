79087231503e140689b7697c7798b2ce819149d4
Shawn Son <shawn9405@gmail.com>
2017-11-19 23:43:36 -0800

    Add eslint along with eslint rules for react native project

--- ReactNativeBasics/src/App.js
+++ ReactNativeBasics/src/App.js
@@ -1,20 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
+  ios: `Press Cmd+R to reload,${'\n'}Cmd+D or shake for dev menu`,
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
@@ -24,7 +12,7 @@ export class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!{"\n"}
+          Welcome to React Native!{'\n'}
           This is a practice application.
         </Text>
         <Text style={styles.instructions}>
